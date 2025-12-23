434dad95222ce7bcbd3ba7d9054e621af4d71bdc
gogavejyoti <gogavejyoti@gmail.com>
2025-12-23 22:38:25 +0530

    Update luckysheetDiagnosis.js

--- luckysheetDiagnosis.js
+++ luckysheetDiagnosis.js
@@ -1,198 +1,159 @@
-            updatecell: function (e, n, t, l = !0) {
-                let a = this
-                    , o = $("#luckysheet-rich-text-editor")
-                    , s = o.text()
-                    , u = o.html();
-                if (a.rangetosheet != null && a.rangetosheet != h.currentSheetIndex && ve.changeSheetExec(a.rangetosheet),
-                    !bn(e, n, h.currentSheetIndex))
-                    return;
-                if (Xe.dataVerification != null) {
-                    let _ = Xe.dataVerification[e + "_" + n];
-                    if (_ != null && _.prohibitInput && !Xe.validateCellData(s, _)) {
-                        let T = Xe.getFailureText(_);
-                        U.info(T, ""),
-                            a.cancelNormalSelected();
-                        return
-                    }
-                }
-                let d = h.flowdata[e][n]
-                    , f = JSON.stringify(d)
-                    , m = xl(d)
-                    , g = s.slice(0, 1) != "=" && u.substr(0, 5) == "<span"
-                    , y = !1;
-                if (!g && s && s.length > 0) {
-                    let _ = s.replace(/\r\n/g, "_x000D_").replace(/&#13;&#10;/g, "_x000D_").replace(/\r/g, "_x000D_").replace(/\n/g, "_x000D_").split("_x000D_");
-                    _.length > 1 && (y = !0,
-                        g = !0,
-                        s = _.join(`\r
-`))
-                }
-                if (!t && !g && m ? (delete d.ct.s,
-                    d.ct.t = "g",
-                    d.ct.fa = "General",
-                    t = "") : g && (L(d) != "object" && (d = {}),
-                        delete d.f,
-                        delete d.v,
-                        delete d.m,
-                        d.ct == null && (d.ct = {},
-                            d.ct.fa = "General"),
-                        d.ct.t = "inlineStr",
-                        d.ct.s = Hd(o.find("span")),
-                        y && (d.ct.s = [{
-                            v: s
-                        }])),
-                    t = t || o.text(),
-                    !Je.createHookFunction("cellUpdateBefore", e, n, t, l)) {
-                    a.cancelNormalSelected();
-                    return
-                }
-                if (!g) {
-                    if (fe(t) && !m) {
-                        if (d == null || fe(d.v) && d.spl == null && d.f == null) {
-                            a.cancelNormalSelected();
-                            return
-                        }
-                    } else if (d != null && d.qp != 1) {
-                        //if (L(d) == "object" && (t == d.f || t == d.v || t == d.m)) {
-                        //    a.cancelNormalSelected();
-                        //    return
-                        //} else 
-                        
-                        if (t == d) {
-                            a.cancelNormalSelected();
-                            return
-                        }
-                    }
-                    L(t) == "string" && t.slice(0, 1) == "=" && t.length > 1 || L(d) == "object" && d.ct != null && d.ct.fa != null && d.ct.fa != "@" && !fe(t) && (delete d.m,
-                        d.f != null && (delete d.f,
-                            delete d.spl))
-                }
-                window.luckysheet_getcelldata_cache = null;
-                let v = !0
-                    , k = we.deepCopyFlowData(h.flowdata)
-                    , b = null;
-                if (L(d) == "object") {
-                    if (!g)
-                        if (L(t) == "string" && t.slice(0, 1) == "=" && t.length > 1) {
-                            let _ = a.execfunction(t, e, n, void 0, !0);
-                            if (v = !1,
-                                d = $.extend(!0, {}, k[e][n]),
-                                d.v = _[1],
-                                d.f = _[2],
-                                _.length == 4 && _[3].type == "sparklines") {
-                                delete d.m,
-                                    delete d.v;
-                                let T = _[3].data;
-                                L(T) == "array" && L(T[0]) != "object" ? d.v = T[0] : d.spl = _[3].data
-                            } else
-                                _.length == 4 && _[3].type == "dynamicArrayItem" && (b = _[3].data)
-                        } else if (L(t) == "object") {
-                            let _ = t.f;
-                            if (L(_) == "string" && _.slice(0, 1) == "=" && _.length > 1) {
-                                let T = a.execfunction(_, e, n, void 0, !0);
-                                if (v = !1,
-                                    d = $.extend(!0, {}, k[e][n]),
-                                    d.v = T[1],
-                                    d.f = T[2],
-                                    T.length == 4 && T[3].type == "sparklines") {
-                                    delete d.m,
-                                        delete d.v;
-                                    let A = T[3].data;
-                                    L(A) == "array" && L(A[0]) != "object" ? d.v = A[0] : d.spl = T[3].data
-                                } else
-                                    T.length == 4 && T[3].type == "dynamicArrayItem" && (b = T[3].data)
-                            } else
-                                for (let T in t)
-                                    d[T] = t[T]
-                        } else
-                            a.delFunctionGroup(e, n),
-                                a.execFunctionGroup(e, n, t),
-                                v = !1,
-                                d = $.extend(!0, {}, k[e][n]),
-                                d.v = t,
-                                delete d.f,
-                                delete d.spl,
-                                d.qp == 1 && ("" + t).substr(0, 1) != "'" && (d.qp = 0,
-                                    d.ct != null && (d.ct.fa = "General",
-                                        d.ct.t = "n"));
-                    t = d
-                } else if (L(t) == "string" && t.slice(0, 1) == "=" && t.length > 1) {
-                    let _ = a.execfunction(t, e, n, void 0, !0);
-                    if (v = !1,
-                        t = {
-                            v: _[1],
-                            f: _[2]
-                        },
-                        _.length == 4 && _[3].type == "sparklines") {
-                        let T = _[3].data;
-                        L(T) == "array" && L(T[0]) != "object" ? t.v = T[0] : t.spl = _[3].data
-                    } else
-                        _.length == 4 && _[3].type == "dynamicArrayItem" && (b = _[3].data)
-                } else if (L(t) == "object") {
-                    let _ = t.f;
-                    if (L(_) == "string" && _.slice(0, 1) == "=" && _.length > 1) {
-                        let T = a.execfunction(_, e, n, void 0, !0);
-                        if (v = !1,
-                            t.v = T[1],
-                            t.f = T[2],
-                            T.length == 4 && T[3].type == "sparklines") {
-                            let A = T[3].data;
-                            L(A) == "array" && L(A[0]) != "object" ? t.v = A[0] : t.spl = T[3].data
-                        } else
-                            T.length == 4 && T[3].type == "dynamicArrayItem" && (b = T[3].data)
-                    } else {
-                        let T = d;
-                        t.v == null && (t.v = T)
-                    }
-                } else
-                    a.delFunctionGroup(e, n),
-                        a.execFunctionGroup(e, n, t),
-                        v = !1;
-                At(e, n, k, t),
-                    a.cancelNormalSelected();
-                let w = !1
-                    , x = $.extend(!0, {}, Ft()[K(h.currentSheetIndex)].config);
+updatecell: function (e, n, t, l = !0) {
+    let a = this,
+        o = $("#luckysheet-rich-text-editor"),
+        s = o.text(),
+        u = o.html();
 
-                if (x.rowlen == null && (x.rowlen = {}),
-                    k[e][n].tb == "2" && k[e][n].v != null || xl(k[e][n]) && typeof k[e][n].mc == "undefined") {
-                    let _ = h.defaultrowlen
-                        , T = $("#luckysheetTableContent").get(0).getContext("2d");
-                    if (!(x.customHeight && x.customHeight[e] == 1)) {
-                        let A = Rt(n)[1] - Rt(n)[0] - 2
-                            , R = Tr(k[e][n], T, {
-                                r: e,
-                                c: n,
-                                cellWidth: A
-                            })
-                            , I = _;
-                        R != null && (I = R.textHeightAll + 2),
-                            I > _ && (x.rowlen[e] = I,
-                                w = !0)
-                    }
+    // Switch sheet if required
+    if (a.rangetosheet != null && a.rangetosheet != h.currentSheetIndex) {
+        ve.changeSheetExec(a.rangetosheet);
+    }
+
+    if (!bn(e, n, h.currentSheetIndex)) return;
+
+    // ---------------- Data validation ----------------
+    if (Xe.dataVerification != null) {
+        let dv = Xe.dataVerification[e + "_" + n];
+        if (dv != null && dv.prohibitInput && !Xe.validateCellData(s, dv)) {
+            U.info(Xe.getFailureText(dv), "");
+            a.cancelNormalSelected();
+            return;
+        }
+    }
+
+    let oldCell = h.flowdata[e][n];
+    let oldSnapshot = JSON.stringify(oldCell);
+    let isFormulaCell = xl(oldCell);
+
+    // ---------------- Inline string handling ----------------
+    let isInlineStr = s.slice(0, 1) !== "=" && u.substr(0, 5) === "<span";
+    let hasNewLine = false;
+
+    if (!isInlineStr && s && s.length > 0) {
+        let arr = s
+            .replace(/\r\n/g, "_x000D_")
+            .replace(/&#13;&#10;/g, "_x000D_")
+            .replace(/\r/g, "_x000D_")
+            .replace(/\n/g, "_x000D_")
+            .split("_x000D_");
+
+        if (arr.length > 1) {
+            hasNewLine = true;
+            isInlineStr = true;
+            s = arr.join("\r\n");
+        }
+    }
+
+    // ---------------- Prepare editable data ----------------
+    let data = we.deepCopyFlowData(h.flowdata);
+    let cell = data[e][n] || {};
+
+    // ---------------- Normalize input ----------------
+    if (isInlineStr) {
+        // Inline text
+        delete cell.f;
+        delete cell.v;
+        delete cell.m;
+        cell.ct = cell.ct || { fa: "General" };
+        cell.ct.t = "inlineStr";
+        cell.ct.s = Hd(o.find("span"));
+        if (hasNewLine) {
+            cell.ct.s = [{ v: s }];
+        }
+    } else {
+        t = t || s;
+
+        // ---------------- Formula input ----------------
+        if (typeof t === "string" && t.startsWith("=") && t.length > 1) {
+            // 🔑 Normalize formula ONLY (NO calculation)
+            let r = a.execfunction(t, e, n, void 0, false);
+            cell.f = r[2];
+            cell.v = null;
+            cell.m = null;
+            delete cell.spl;
+        }
+        // ---------------- Object input ----------------
+        else if (typeof t === "object") {
+            for (let k in t) cell[k] = t[k];
+        }
+        // ---------------- Value input ----------------
+        else {
+            delete cell.f;
+            delete cell.spl;
+            cell.v = t;
+            cell.qp === 1 && (cell.qp = 0);
+        }
+    }
+
+    // ---------------- Hook before update ----------------
+    if (!Je.createHookFunction("cellUpdateBefore", e, n, t, l)) {
+        a.cancelNormalSelected();
+        return;
+    }
+
+    // ---------------- Apply change ----------------
+    data[e][n] = cell;
+    At(e, n, data, cell);
+
+    // ---------------- Row height recalculation ----------------
+    let cfg = $.extend(true, {}, Ft()[K(h.currentSheetIndex)].config);
+    cfg.rowlen == null && (cfg.rowlen = {});
+    let rowChanged = false;
+
+    if (cell.tb === "2" && cell.v != null || xl(cell) && cell.mc == null) {
+        let ctx = $("#luckysheetTableContent").get(0).getContext("2d");
+        let defaultH = h.defaultrowlen;
+        if (!(cfg.customHeight && cfg.customHeight[e] === 1)) {
+            let width = Rt(n)[1] - Rt(n)[0] - 2;
+            let size = Tr(cell, ctx, { r: e, c: n, cellWidth: width });
+            if (size && size.textHeightAll + 2 > defaultH) {
+                cfg.rowlen[e] = size.textHeightAll + 2;
+                rowChanged = true;
+            }
+        }
+    }
+
+    // ---------------- Commit to sheet ----------------
+    let params = rowChanged ? { cfg: cfg, RowlChange: true } : {};
+    Ye(data, [{ row: [e, e], column: [n, n] }], params, false);
+    a.cancelNormalSelected();
+
+    // ---------------- Deterministic calculation pass ----------------
+    try {
+        const sheet = luckysheet.getSheet();
+        const sheetData = sheet.data;
+
+        // 🔥 rebuild calcChain INLINE
+        sheet.calcChain = [];
+
+        for (let r = 0; r < sheetData.length; r++) {
+            for (let c = 0; c < sheetData[r].length; c++) {
+                let ce = sheetData[r][c];
+                if (ce && ce.f) {
+                    Ucv(sheet, r, c, ce.f, false); // register
                 }
-                let C = null;
-                b && (C = $.extend(!0, [], this.insertUpdateDynamicArray(b)));
-                let S = {
-                    dynamicArray: C
-                };
-                if (w && (S = {
-                    cfg: x,
-                    dynamicArray: C,
-                    RowlChange: w
-                }),
-                    setTimeout(() => {
-                        Je.createHookFunction("cellUpdated", e, n, JSON.parse(f), h.flowdata[e][n], l)
-                    }
-                        , 0),
-                    l)
-                    Ye(k, [{
-                        row: [e, e],
-                        column: [n, n]
-                    }], S, v),
-                        a.execFunctionGlobalData = null;
-                else
-                    return {
-                        data: k,
-                        allParam: S
-                    }
-            },
+            }
+        }
+
+        for (let i = 0; i < sheet.calcChain.length; i++) {
+            let node = sheet.calcChain[i];
+            if (node && node.func) {
+                Ucv(sheet, node.r, node.c, node.func[2], true);
+            }
+        }
+    } catch (err) {
+        console.error("updatecell calc failed", err);
+    }
+
+    // ---------------- Hook after update ----------------
+    setTimeout(() => {
+        Je.createHookFunction(
+            "cellUpdated",
+            e,
+            n,
+            JSON.parse(oldSnapshot),
+            h.flowdata[e][n],
+            l
+        );
+    }, 0);
+}
